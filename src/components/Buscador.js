c0df62eaac57e86413e1510ad33551e0dc7c7173
Drezeenebe <pablo.ahumada01@gmail.com>
2024-01-24 22:45:08 -0300

    88-Buscador componente sin datos encontrados

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export const Buscador = ({listMovies,setListMovies}) => {
     
     const [search,setSearch] = useState('')
+    const [notFound,setNotFound] = useState(false)
 
     const searchMovie = e =>{
         //Crear estado y actualizarlo
@@ -14,6 +15,9 @@ export const Buscador = ({listMovies,setListMovies}) => {
 
         if(search.length<=2 || moviesResult.length===0){
             moviesResult = JSON.parse(localStorage.getItem('movies'))
+            setNotFound(true)
+        }else{
+            setNotFound(false)
         }
 
         setListMovies(moviesResult)
@@ -22,12 +26,16 @@ export const Buscador = ({listMovies,setListMovies}) => {
         <>
             <div className="search">
                 <h3 className="title">Buscador</h3>
+                { (notFound && search.length>2 )&&  (
+                    <span className='notFound'>No se ha encontrado coincidencias</span>
+                )}
                 <form action="">
                     <input type="text"
                     id='search_field'
                     name='search_movie'
                     autoComplete='off'
                     value={search}
+                    placeholder='Minimo de 3 caracteres'
                     onChange={e=>searchMovie(e)} />
                     <button id='search'>Buscar</button>
                 </form>
