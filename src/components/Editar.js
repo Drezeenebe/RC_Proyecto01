8f58213f13459070eef70c145f48c2d280fa545f
Drezeenebe <pablo.ahumada01@gmail.com>
2024-01-24 21:53:42 -0300

    84-Formulario edicion de pelicula

--- src/components/Editar.js
+++ src/components/Editar.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 export const Editar = () => {
-  return (
-    <>
-        <h1>Formulario editar</h1>
-    </>
-  )
+
+    const title_component = 'Editar Pelicula'
+    return (
+        <div className='edit_form'>
+            <h3>{title_component}</h3>
+            <form>
+                <input type="text"
+                    name='title'
+                    className='title_edit'
+                    defaultValue="titulo original" />
+                <textarea
+                    name='descript'
+                    defaultValue="Descripcion original"
+                    className='descript_edit'
+                />
+                <input type="text" className='edit' value="Actualizar" />
+            </form>
+        </div>
+    )
 }
